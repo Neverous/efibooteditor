47f9a8c908ddd0de5445f6973cead25fe09bd1d6
Maciej Szeptuch <765629+Neverous@users.noreply.github.com>
2026-05-03 20:17:31 +0200

    fix icons fallback, add simple visual test (#523)
    * update clang linter
    
    * fix icons fallback, add simple visual test

---
+++ tests/testefibooteditor.cpp
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+#include "compat.h"
+
+#include <QIcon>
+#include <QTest>
+
+#include "efibooteditor.h"
+#include "main.h"
+
+class TestEFIBootEditor: public QObject
+{
+    Q_OBJECT
+
+public:
+    TestEFIBootEditor() = default;
+
+private Q_SLOTS:
+    void testMainWindow() const;
+};
+
+void TestEFIBootEditor::testMainWindow() const
+{
+    EFIBootEditor gui(std::nullopt);
+    gui.show();
+
+    QCOMPARE(QTest::qWaitForWindowExposed(&gui), true);
+    QTest::qWait(500);
+
+    QPixmap snapshot = gui.grab();
+    QCOMPARE(snapshot.save("screenshot.png"), true);
+}
+
+auto main(int argc, char *argv[]) -> int
+{
+    QApplication app(argc, argv);
+    setupApplication();
+    (void)setupTranslations();
+    setupStyle();
+
+    TestEFIBootEditor test;
+    return QTest::qExec(&test, argc, argv);
+}
+
+#include "testefibooteditor.moc"
